ac9193035c19db39f47dc7d22a61e4ade1b79b7b
Handsome <2420850076@qq.com>
2021-11-10 19:43:36 +0800

    master2 commit

--- test2/src/GitClass.java
+++ test2/src/GitClass.java
@@ -1,6 +1,6 @@
 public class GitClass {
     public static void main(String[] args) {
-        System.out.println("this is a program for testing git!");
+        System.out.println("this is a program for testing git-mater!");
         System.out.println("this is a program for testing git2!");
         System.out.println("this is a program for testing git3!");
         System.out.println("this is a program for testing git4-hotfix!");
