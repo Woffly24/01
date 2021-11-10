51a8c9a8cdb3bff89d7d8d81a36519daea4eaa78
Handsome <2420850076@qq.com>
2021-11-10 19:34:26 +0800

    hot-fix1 commit

--- test2/src/GitClass.java
+++ test2/src/GitClass.java
@@ -3,6 +3,6 @@ public class GitClass {
         System.out.println("this is a program for testing git!");
         System.out.println("this is a program for testing git2!");
         System.out.println("this is a program for testing git3!");
-        System.out.println("this is a program for testing git4!");
+        System.out.println("this is a program for testing git4-hotfix!");
     }
 }
