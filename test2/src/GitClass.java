396b7510d22bec924109f8dade11cc47568a4252
Handsome <2420850076@qq.com>
2021-11-10 18:51:28 +0800

    second commit

---
+++ test2/src/GitClass.java
@@ -0,0 +1,6 @@
+public class GitClass {
+    public static void main(String[] args) {
+        System.out.println("this is a program for testing git!");
+        System.out.println("this is a program for testing git2!");
+    }
+}
