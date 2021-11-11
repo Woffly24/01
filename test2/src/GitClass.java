90550e356ebd550079fb624b0962ab930eef1a7a
Handsome <2420850076@qq.com>
2021-11-11 16:35:44 +0800

    push master

--- test2/src/GitClass.java
+++ test2/src/GitClass.java
@@ -1,8 +1,21 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class GitClass {
     public static void main(String[] args) {
-        System.out.println("this is a program for testing git-mater!");
-        System.out.println("this is a program for testing git2!");
-        System.out.println("this is a program for testing git3!");
-        System.out.println("this is a program for testing git4-hotfix!");
+        List<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
+        arr.add(5);
+
+        Iterator<Integer> iterator = arr.iterator();
+        while(iterator.hasNext()){
+            iterator.next();
+            iterator.remove();
+        }
+        System.out.println("this is a change on IDEA");
     }
 }
